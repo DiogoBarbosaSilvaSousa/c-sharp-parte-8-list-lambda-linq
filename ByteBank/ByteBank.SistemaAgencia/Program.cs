9cb6b7e7d48014b700c97c6e10936318a1e77d94
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-08-31 14:51:46 -0300

    ListT e métodos de extensão

---
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> idades = new List<int>();
+
+            idades.Add(1);
+            idades.Add(5);
+            idades.Add(14);
+            idades.Add(25);
+            idades.Add(38);
+            idades.Add(61);
+
+            // ListExtensoes.AdicionarVarios(idades, 1, 5687, 1987, 1567);
+
+            idades.AdicionarVarios(5, 448, 7898, 4564);
+
+            for(int i =0; i < idades.Count; i++)
+            {
+                Console.WriteLine(idades[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
